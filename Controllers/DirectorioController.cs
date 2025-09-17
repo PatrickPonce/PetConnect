9d980a399c109ad5313e56d443c397872d2de994
Lymixe <peraltacenepoemily@gmail.com>
2025-09-17 01:19:14 -0500

    rama servicio-veterinaria

--- Controllers/DirectorioController.cs
+++ Controllers/DirectorioController.cs
@@ -1,12 +1,35 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PetConnect.Data;
 
-namespace PetConnect.Controllers
+namespace PetConnect.Controllers;
+
+public class DirectorioController : Controller
 {
-    public class DirectorioController : Controller
+    private readonly ApplicationDbContext _context;
+
+    public DirectorioController(ApplicationDbContext context)
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
+        _context = context;
     }
+
+public async Task<IActionResult> Veterinaria()
+{
+    var serviciosConCategoria = await (
+        from servicio in _context.Servicios
+        join categoria in _context.Categorias on servicio.CategoriaId equals categoria.Id
+        select new 
+        {
+            Id = servicio.Id,
+            Nombre = servicio.Nombre,
+            ImagenUrl = servicio.ImagenPrincipalUrl,
+            Direccion = servicio.Direccion,
+            NombreCategoria = categoria.Nombre 
+        }
+    ).ToListAsync();
+
+    ViewBag.Servicios = serviciosConCategoria;
+    
+    return View();
+}
 }

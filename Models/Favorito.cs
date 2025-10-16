fa3ac2dc1a41578bfc0cdd0ce89b77a096704589
kiaraLinares19 <kiaralinaresestivariz@gmail.com>
2025-10-16 01:45:21 -0500

    Models favorito

---
+++ Models/Favorito.cs
@@ -0,0 +1,22 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Identity; 
+
+namespace PetConnect.Models
+{
+    public class Favorito
+    {
+        [Required]
+        public string UsuarioId { get; set; }
+        
+        // 2. Clave Foránea a la Noticia
+        [Required]
+        public int NoticiaId { get; set; }
+
+        public IdentityUser Usuario { get; set; } = default!;
+        public Noticia Noticia { get; set; } = default!;
+
+        public DateTime FechaAgregado { get; set; } = DateTime.UtcNow;
+    }
+}

3c2ad656534aa9272fca36df04560fb1117f39dc
Seeebaastian <davidalaba267@gmail.com>
2025-10-23 21:39:13 -0500

    Configuracion

---
+++ Configuration/GoogleMapsConfig.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PetConnect.Configuration
+{
+    public class GoogleMapsConfig
+    {
+        public string? ApiKey { get; set; }
+    }
+}

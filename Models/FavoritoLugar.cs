8ecc5c3967f9238dfe46707a4775a77e69878d3d
Lymixe <peraltacenepoemily@gmail.com>
2025-10-24 02:00:22 -0500

    Modificación Guarderias Favoritas

--- Models/FavoritoLugar.cs
+++ Models/FavoritoLugar.cs
@@ -5,19 +5,18 @@ using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.AspNetCore.Identity;
 
+
+
 namespace PetConnect.Models
 {
     public class FavoritoLugar
     {
-        public int Id { get; set; }
-
-        // Foreign Key para el Usuario
-        public string UsuarioId { get; set; }
-        [ForeignKey("UsuarioId")]
-        public IdentityUser Usuario { get; set; }
-
         // Foreign Key para el LugarPetFriendly
         public int LugarPetFriendlyId { get; set; }
-        public LugarPetFriendly LugarPetFriendly { get; set; }
+        public virtual LugarPetFriendly LugarPetFriendly { get; set; }
+        
+        // Foreign Key para el Usuario
+        public string UsuarioId { get; set; }
+        public virtual IdentityUser Usuario { get; set; }
     }
 }

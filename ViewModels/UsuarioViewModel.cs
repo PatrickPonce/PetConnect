9ffea74fc8a2e27d9d6e9dcb08056d38bfe931a0
PatrickPonce <patrick_ponce1@usmp.pe>
2025-10-31 01:41:27 -0500

    listar usuarios

---
+++ ViewModels/UsuarioViewModel.cs
@@ -0,0 +1,12 @@
+namespace PetConnect.ViewModels
+{
+    public class UsuarioViewModel
+    {
+        public required string Id { get; set; }
+        public required string NombreUsuario { get; set; }
+        public string? Email { get; set; }
+        public string? NumeroTelefono { get; set; }
+        public string? ProfilePictureUrl { get; set; }
+        public bool IsLockedOut { get; set; }
+    }
+}
